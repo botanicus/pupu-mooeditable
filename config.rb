1334c12272e97ac9de80d4501111e763d4f46b6d
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2008-12-02 16:17:09 +0100

    Initial import

---
+++ config.rb
@@ -0,0 +1,14 @@
+# dependencies of the pupu
+#dependencies :mootools
+
+# javascripts for loading
+javascripts "mooeditable", :in => "lib"
+
+# parameters of plugin
+# it will be blank in most cases
+#parameter :request, :optional => ["local", "ajax"] do |type|
+#  javascript "lib/autocompleter.#{type}"
+#end
+
+# you can put there message which will be displayed after installation
+#message "foo"
